f77fbcbf632179fc4c9807eda5bc879d245409f6
cr9ver <cr9vercloud@gmail.com>
2023-08-05 13:54:15 +0200

    created two methods to retrieve list of users

--- IAM/ListAllUsers.py
+++ IAM/ListAllUsers.py
@@ -1,18 +1,28 @@
 import boto3
 
+#Method1
+# def all_users():
+#     iam = boto3.client('iam')
 
-def all_users():
-    iam = boto3.client('iam')
+#     paginator = iam.get_paginator('list_users')
 
-    paginator = iam.get_paginator('list_users')
+#     for response in paginator.paginate():
+#         for user in response['Users']:
+#             username =  user['UserName']
+#             Arn = user['Arn']
+#             print(f'Username: {username} Arn: {Arn}')
 
-    for response in paginator.paginate():
-        for user in response['Users']:
-            username =  user['UserName']
-            Arn = user['Arn']
-            print(f'Username: {username} Arn: {Arn}')
+# all_users()
 
-all_users()
+#Method2
+iam = boto3.client('iam')
+responselist = iam.list_users()
+
+
+for username in responselist['Users']:
+    user = username['UserName']
+    arn = username['Arn']
+    print(f'{user}: {arn}')
 
 
 
